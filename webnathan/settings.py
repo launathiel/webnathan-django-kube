a17b859f26dff4a48f368f78616130b3022efb35
Laurentius Nathaniel <launathiel@gmail.com>
2021-06-15 12:29:19 +0700

    edit docker image version on kube

--- webnathan/settings.py
+++ webnathan/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
